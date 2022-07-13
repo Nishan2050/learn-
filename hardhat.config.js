d0261c4406052ddcfe37ca0794968c0b2286605c
Nishan <1nishansingh@gmail.com>
2022-07-13 07:47:39 -0600

    messgae for learn

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-ethers');
+
+
+// hardhat.config.js
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
+
+module.exports = {
+  solidity: "0.8.4",
+     networks: {
+       rinkeby: {
+        url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+         accounts: { mnemonic: mnemonic },
+       },
+     },
+  
+};
+
+
+
+
+
+
